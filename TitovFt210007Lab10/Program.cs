d12d9050dc770c46c42fa8b376e6e665087d40fa
YWKBM <ywkbm1973@gmail.com>
2023-01-10 13:56:25 +0500

    Added dictionaries with langs, function to choose lang, funcionto choose message

--- TitovFt210007Lab10/Program.cs
+++ TitovFt210007Lab10/Program.cs
@@ -1,9 +1,51 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace TitovFt210007Lab10
 {
     internal class Program
-    {       
+    {
+        //Dictionary<string, int> messages = new Dictionary<string, string>
+        //{
+        //    {"Enter sum", "Введите сумму" },
+        //    {"Quantity of bills to pay", "Колличество купюр для оплаты" },
+        //    {"Wrong input data", "Неправильны входные данные" }
+        //};
+
+        Dictionary<int, string> Engessages = new Dictionary<int, string>
+        {
+            {0, "Enter sum" },
+            {1, "Quantity of bills to pay"},
+            {2, "Wrong input data" }
+        };
+
+        Dictionary<int, string> ruMessages = new Dictionary<int, string>
+        {
+            {0, "Введите сумму" },
+            {1, "Колличество купюр для оплаты" },
+            {2, "Неправильны входные данные" }
+        };
+
+
+
+
+        static void ChoseLang(string lang)
+        {
+            Console.WriteLine("Choose language: (Enter \"ru\" or \"eng\")");
+            lang = Console.ReadLine();
+        }
+
+        static string SendMessage(string lang, int currentMessage)
+        {
+            string msg = null;
+            if (lang == "ru")
+            {
+                
+            }
+            return msg;
+        }
+
         static int BillsAnalyses(int n)
         {
             int count = 0;
